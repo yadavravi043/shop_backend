c0c23ae23f0645373839e387ef8b1f8eb49765c4
yadavravi043 <ravi18016925@gmail.com>
2022-10-20 19:49:32 +0530

    models added

---
+++ models/productsModel.js
@@ -0,0 +1,68 @@
+const mongoose=require('mongoose');
+
+const reviewSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    rating:{
+        type:Number,
+        required:true,
+    },
+    comment:{
+        type:String,
+        required:true,
+    },
+},{timestamp:true})
+
+const productSchema=mongoose.Schema({
+user:{
+    type:mongoose.Schema.Types.ObjectId,
+     required:true,
+     ref :'User'
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    brand:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    rating:{
+        type:Number,
+        required:true,
+        default:0
+    },
+//we can make seprate model for this but this small enough so we can do like this also
+    reviews:[reciewSchema],
+    numReviews:{
+        type:Number,
+        required:true,
+        default:0,
+    },
+    price:{
+        type:Number,
+        required:true,
+        default:0,
+    },
+    countInStock:{
+        type:Number,
+        required:true,
+        default:0,
+    }
+},{timestamp:true})
+const Product=mongoose.model('Product',productSchema)
+export default Product;
---
+++ models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose=require('mongoose');
+const userSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false,
+    }
+},{timestamp:true})
+const User=mongoose.model('User',userSchema)
+export default User;
