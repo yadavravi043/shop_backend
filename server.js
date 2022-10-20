1968bcfb05531b895d1fa148eaa5251b7f86197b
yadavravi043 <ravi18016925@gmail.com>
2022-10-20 18:06:04 +0530

    changes

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const express =require('express')
 const port=process.env.PORT||5000
 const app = express()
 const products = require('./data/products')
-
-
+const  mongoose =require('mongoose')
+const colors=require('colors')
+require("dotenv").config();
 
 app.get('/api',(req,res)=>{
 res.end('server is running')
@@ -16,8 +17,12 @@ app.get('/api/products/:id',(req,res)=>{
     const product=products.find(p=>p._id ===req.params.id)
     res.json(product)
 })
-
+const db=process.env.DATABASE_ATLAS
+mongoose
+.connect(db)
+.then(()=>console.log(`database connected successfully`.cyan.underline))
+.catch(()=>console.log(`database connection failed`.red.underline))
 
 app.listen(port,()=>{
-    console.log(`server is running on port:${port}`)
+    console.log(`server is running on port:${port}`.cyan)
 })
