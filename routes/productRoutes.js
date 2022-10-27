49ddf1380cc0290722f7ab1263811d8f55b66f86
yadavravi043 <ravi18016925@gmail.com>
2022-10-27 20:51:38 +0530

    products fetch from database

---
+++ routes/productRoutes.js
@@ -0,0 +1,21 @@
+const express=require('express')
+const asyncHandler=require('express-async-handler')
+const Product =require('../models/productsModel')
+const router=express.Router()
+     //we can use try catch but we use inbuilt asyncHandler
+     router.get('/',  asyncHandler(async(req,res)=>{
+        const products= await Product.find({})
+        res.json(products)
+     }))
+    
+    router.get('/:id',asyncHandler(async(req,res)=>{
+        const product= await Product.findById(req.params.id)
+        if(product){
+
+            res.json(product)
+        }
+        else{
+            res.status(400).json({message:'product not found'})
+        }
+    }))
+module.exports=router
--- server.js
+++ server.js
@@ -2,22 +2,26 @@ const { json } = require('express')
 const express =require('express')
 const port=process.env.PORT||5000
 const app = express()
-const products = require('./data/products')
+//const products = require('./data/products')
 const  mongoose =require('mongoose')
 const connectDB=require('./config/db')
 const colors=require('colors')
 require("dotenv").config();
 
+const productRoutes=require('./routes/productRoutes')
 app.get('/api',(req,res)=>{
 res.end('server is running')
 })
-app.get('/api/products',(req,res)=>{
-    res.json(products)
-})
-app.get('/api/products/:id',(req,res)=>{
-    const product=products.find(p=>p._id ===req.params.id)
-    res.json(product)
-})
+//  app.get('/api/products',(req,res)=>{
+//     res.json(products)
+//  })
+
+// app.get('/api/products/:id',(req,res)=>{
+//     const product=products.find(p=>p._id ===req.params.id)
+//     res.json(product)
+// })
+
+app.use('/api/products',productRoutes)
 const db=process.env.DATABASE_ATLAS
 mongoose
 .connect(db)
