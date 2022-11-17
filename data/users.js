19589588181e34efcd6c4eedde615b14b47d976a
yadavravi043 <ravi18016925@gmail.com>
2022-11-17 09:20:06 +0530

    user bcrypt password

--- data/users.js
+++ data/users.js
@@ -3,22 +3,22 @@ const users=[
     {
         name:'Admin user',
         email:'Admin@gmail.com',
-        password:123456,
-        //password:bcrypt.hashsync('123456@',10),
+        //password:123456,
+        password:bcrypt.hashsync('123456@',10),
         isAdmin:true
     },
     {
         name:'Ravi',
         email:'ravi@gmail.com',
-        password:123456,
-        //password:bcrypt.hashsync('123456@',10),
+        //password:123456,
+        password:bcrypt.hashsync('123456@',10),
         isAdmin:true
     },
     {
         name:'Archana',
         email:'archu@gmail.com',
-        password:123456,
-        //password:bcrypt.hashsync('123456@',10),
+        //password:123456,
+      password:bcrypt.hashsync('123456@',10),
         isAdmin:true
     }
 ]
