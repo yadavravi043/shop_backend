993284984e3b8b327d5b38eb7ee8b3647b35bd9a
yadavravi043 <ravi18016925@gmail.com>
2022-11-23 17:21:14 +0530

    i forget user in ordercontroller

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -2,22 +2,23 @@ const asyncHandler = require("express-async-handler");
 const Order = require("../models/orderModel");
 
 module.exports.addOrderItems = asyncHandler(async (req, res) => {
-    const {orderItems,
+    const {
+        orderItems,
         shippingAddress,
         paymentMethod,
         itemsPrice,
         taxPrice,
         shippingPrice,
-        totalPrice}=req.body;
+        totalPrice}=req.body
       
         if(orderItems && orderItems.length===0){
           res.status(400)
           throw new Error('no order items')
-          return
         }
         else{
             const order=new Order({
                 orderItems,
+                user:req.user._id,
                 shippingAddress,
                 paymentMethod,
                 itemsPrice,
--- models/orderModel.js
+++ models/orderModel.js
@@ -3,7 +3,7 @@ const orderSchema=mongoose.Schema({
     user:{
         type:mongoose.Schema.Types.ObjectId,
         required:true,
-        ref:'User'
+        ref:'User',
     },
     orderItems:[
         {
